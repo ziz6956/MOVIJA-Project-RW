bc06bd07409efad8812c239705a44ea3dbbc9880
ziz69 <ziz6956@yandex.ru>
2026-03-17 19:21:17 +0300

    feat: add xray-log-analyzer roadmap and update node menu

--- setup_node.sh
+++ setup_node.sh
@@ -43,7 +43,8 @@ show_node_menu() {
     echo "3) Только восстановление сертификатов (из архива)"
     echo "4) Поиск лучшего SNI для Reality (Сканер)"
     echo "5) Диагностика VPS (Fusion Monster / goecs)"
-    echo "6) Выход"
+    echo "6) Информация об анализаторе логов (Xray Log Analyzer)"
+    echo "7) Выход"
     echo -e "------------------------------------------"
     read -p "Выберите действие [1-6]: " main_choice
 
@@ -53,7 +54,8 @@ show_node_menu() {
         3) source "$MODULES_DIR/08_backup_manager.sh"; restore_certificates ;;
         4) source "$MODULES_DIR/09_reality_scanner.sh"; run_reality_scanner ;;
         5) source "$MODULES_DIR/10_vps_check.sh"; run_vps_check ;;
-        6) exit 0 ;;
+        6) source "$MODULES_DIR/11_xray_analyzer.sh"; run_log_analyzer_info ;;
+        7) exit 0 ;;
         *) show_node_menu ;;
     esac
 }
